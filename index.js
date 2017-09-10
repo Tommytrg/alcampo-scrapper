14013adf840f934c65d7c1cc3565b417e0739e72
Tomas Ruiz <tomastrg22@gmail.com>
2017-09-10 22:19:06 +0200

    added pdp_endpoints

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const domain = 'https://www.alcampo.es'
+const jsonifyGetRequest = require('jsonify-get-request');
+
+for (let i = 0; i < 2; i += 1) {
+  const endpoint = `https://wrapapi.com/use/tommytrg/alcampo/prueba1alcampo/0.0.3?page=${i}&wrapAPIKey=Zr9bRkPvvQVS6beAoemHChcfPlUtMk23`;
+  jsonifyGetRequest(endpoint, 'pdp_endpoints.json');
+}
