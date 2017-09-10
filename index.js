9843eb7d62af15b08c7d1f80b7fc9d4229af4c63
Tomas Ruiz <tomastrg22@gmail.com>
2017-09-10 22:26:52 +0200

    pdp_enpoints added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonifyGetRequest = require('jsonify-get-request');
 
-for (let i = 0; i < 2; i += 1) {
+for (let i = 0; i < 405; i += 1) {
   const endpoint = `https://wrapapi.com/use/tommytrg/alcampo/prueba1alcampo/0.0.3?page=${i}&wrapAPIKey=Zr9bRkPvvQVS6beAoemHChcfPlUtMk23`;
   jsonifyGetRequest(endpoint, 'pdp_endpoints.json');
 }
