6dc448e57e8a17f9af28921d05640bf1d914d4c2
Tomas Ruiz <tomastrg22@gmail.com>
2017-09-19 22:50:09 +0200

    beautify

--- get-plp-endpoints.js
+++ get-plp-endpoints.js
@@ -1,14 +1,11 @@
-var Xray = require('x-ray');
-var x = Xray();
+const Xray = require('x-ray');
 
-const endpoint = 'https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Aalcampo&page=0'
+const x = Xray();
 
-x(endpoint, 'h2',
-  [{
-    url: 'a@href'
-  }]
-)
+const endpoint = 'https://www.alcampo.es/compra-online/alimentacion/c/WC10?q=%3Aalcampo&page=0';
+
+x(endpoint, 'h2', [{ url: 'a@href' }])
   .paginate('.pagination a[rel=next]:last-child@href')
   .limit(405)
-  .write('./alcampo.json') //3652 lines === are 1200 products === 50 plp's
+  .write('./alcampo.json');
 
