09aaa9cc4799d942d77303ee2a6e32dd0a6f379f
Tomas Ruiz <tomastrg22@gmail.com>
2017-09-21 00:55:31 +0200

    saved alcampo products

--- get-products-info.js
+++ get-products-info.js
@@ -1,28 +1,61 @@
 const Xray = require('x-ray');
 
 const x = Xray();
+const fs = require('fs');
 
-const endpoint = 'https://www.alcampo.es/compra-online/alimentacion/caldos-pasta-arroz-legumbres-pure/pasta/con-vegetales/barilla-12-penne-tricolor-500-gramos/p/515118';
+function readJSON(name) {
+  return JSON.parse(fs.readFileSync(name, 'utf8'));
+}
 
-x(
-  endpoint,
-  'body',
-  [
-    {
-      ingredients: '.foodIngredients',
-      kj: '.tablaValorContenido',
-      kcal: 'tr+ tr .tablaValorContenido',
-      fats: '.foodFats .kJ',
-      carbohydrates: '.foodCarboHydrates .kJ',
-      proteins: '.foodProteins .kJ',
-      sugar: '.foodSugars .kJ',
-      salt: '.foodSalt .kJ',
-      saturated_fats: '.foodSaturatedFats .kJ',
-      conservation: '.productConservationCoditions',
+function getProductInformation(endpoint) {
+  return new Promise((resolve, reject) => {
+    x(
+      endpoint,
+      'body',
+      [
+        {
+          name: 'h1',
+          brand: '.productTitle',
+          img: '#imageLink img@src',
+          ingredients: '.foodIngredients',
+          kj: '.tablaValorContenido',
+          kcal: 'tr+ tr .tablaValorContenido',
+          fats: '.foodFats .kJ',
+          carbohydrates: '.foodCarboHydrates .kJ',
+          proteins: '.foodProteins .kJ',
+          sugar: '.foodSugars .kJ',
+          salt: '.foodSalt .kJ',
+          saturated_fats: '.foodSaturatedFats .kJ',
+          conservation: '.productConservationCoditions',
 
-    },
-  ],
-)((err, res) => {
-  if (err) console.log(err);
-  console.log(res);
-});
+        },
+      ],
+    )((err, res) => {
+      resolve(res);
+      reject(err);
+    });
+  });
+}
+
+function getProductsInformation() {
+  const endpoints = readJSON('alcampo.json');
+
+  const products = [];
+  for (let i = 10000; i < 11000; i += 1) {
+    if (endpoints[i]) {
+      products.push(getProductInformation(endpoints[i].url));
+    }
+  }
+
+  Promise.all(products)
+    .then((result) => {
+      const information = readJSON('alcampo-products10.json');
+      information.push(result);
+      fs.writeFileSync('alcampo-products11.json', JSON.stringify(information), 'utf8');
+    })
+    .catch((err) => {
+      console.log('err', err);
+    });
+}
+
+getProductsInformation();
